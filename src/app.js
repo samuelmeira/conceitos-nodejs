feb47297a65b0c49253429706045224404ff8e67
Samuel Meira <samuelleandmeira@hotmail.com>
2020-06-24 20:12:24 -0300

    Repositories CRUD and likes feature

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,22 +11,56 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {url, title, techs } = request.body;
+
+  const repository = { id: uuid(), url, title, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {url, title, techs } = request.body;
+
+  const repository = repositories.find(repository => repository.id === id );
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.url = url
+  repository.title = title
+  repository.techs = techs
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send({})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id )
+  if (!repository) {
+    return response.status(400).send();
+  }
+  repository.likes++
+  return response.json(repository);
   // TODO
 });
 
